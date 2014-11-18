dc403382f580fdd88f7cd529b6abd199dc8ef0a8
MNRaju <nookarajumalladi@gmail.com>
2014-11-18 20:23:06 +0530

    this is vertical menu

---
+++ index.php
@@ -0,0 +1,34 @@
+   <link rel="stylesheet" href="styles.css">
+   <script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
+   <script src="script.js"></script>
+
+<div id='cssmenu'>
+<ul>
+  <!-- <li class='active'><a href='#'><span>Home</span></a></li>-->
+   <li class='has-sub'><a href='#'><span>Positions</span></a>
+      <ul style="margin:0;padding:0;">
+         <li><a href='#'><span>Locations</span></a></li>
+         <li><a href='#'><span>Cost Centers</span></a></li>
+		 <li><a href='#'><span>Departments</span></a></li>
+		 <li><a href='#'><span>Types</span></a></li>
+		  <li><a href='#'><span>Required Position Approvals</span></a></li>
+         <li class='last'><a href='#'><span>Default Pay Period</span></a></li>
+      </ul>
+   </li>
+   <li class='has-sub' ><a href='#'><span style="background-color:#0099FF !important">Projects</span></a>
+      <ul style=" background-color:#00CCFF">
+         <li><a href='#'><span>Required Position Approvals</span></a></li>
+         <li class='last'><a href='#'><span>Default Pay Period</span></a></li>
+      </ul>
+   </li>
+   <li class='has-sub'><a href='#'><span>Other</span></a>
+      <ul>
+         <li><a href='#'><span>Required Position Approvals</span></a></li>
+         <li ><a href='#'><span>Default Pay Period</span></a></li>
+		 <li ><a href='#'><span>Insurance Types</span></a></li>
+		 <li class='last'><a href='#'><span>Work Locations</span></a></li>
+      </ul>
+   </li>
+  <!-- <li class='last'><a href='#'><span>Contact</span></a></li>-->
+</ul>
+</div>
